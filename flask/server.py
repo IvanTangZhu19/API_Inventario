a6fc69dd5cc7d2353f0a14698ea5ac235fd55874
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-07-08 13:37:12 -0500

    feat: get products por id

--- flask/server.py
+++ flask/server.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 inventario = [
     {
-        "productId": 1,
+        "productID": 1,
         "name": "Manzana",
         "currentStock": 2,
         "minimunStock": 1,
         "lastUpdated": ""
     },
     {
-        "productId": 2,
+        "productID": 2,
         "name": "Mango",
         "currentStock": 5,
         "minimunStock": 2,
@@ -31,8 +31,30 @@ users = [
 
 
 @app.route("/inventory")
-def inventory():
-    return jsonify({inventario}), 200
+def getProducts():
+    return jsonify(inventario), 200
+
+@app.route("/inventory/<int:productID>")
+def getProductsByID(productID):
+    if (productID is not None):
+        for i in inventario:
+            if i["productID"] == productID :
+                return jsonify(i), 200
+        return jsonify({
+            "error": {
+                "code": "ABC",
+                "message": "ProductoId no encontrado",
+                "details": "Parámetro productId no encontrado en la base de datos"
+            }
+        }), 404 
+    else:
+        return jsonify({
+            "error": {
+                "code": "ABC",
+                "message": "Parámetros incorrectos",
+                "details": "productId incorrecto"
+            }
+        }), 400 
 
 if __name__ == '__main__':
     app.run(debug=True, port=4002)

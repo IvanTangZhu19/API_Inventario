1d9daa6d62a6bc6cea7886a940993397729bdf40
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-07-15 17:05:09 -0500

    feat: Django Ver productos

--- django/inventario/views.py
+++ django/inventario/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import datetime
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hola")
+inventario = [
+    {
+        "productID": 1,
+        "name": "Manzana",
+        "currentStock": 2,
+        "minimunStock": 1,
+        "lastUpdated": datetime.datetime.utcnow()
+    },
+    {
+        "productID": 2,
+        "name": "Mango",
+        "currentStock": 5,
+        "minimunStock": 2,
+        "lastUpdated": datetime.datetime.utcnow()
+    }
+]
+
+def getProducts(request):
+    if request.method == "GET":
+        return JsonResponse(inventario, safe=False)

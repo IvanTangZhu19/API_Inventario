1c04491cc1ff102ff1a342e56cb8f85ab44206b7
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-07-15 17:20:37 -0500

    feat: Django crear producto

--- django/inventario/urls.py
+++ django/inventario/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getProducts),
+    path('', views.Products),
 ]
--- django/inventario/views.py
+++ django/inventario/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import datetime
+import json
+
+productID_ =2
+
+JWT_SECRET = "JWT"
 
 inventario = [
     {
@@ -19,6 +25,43 @@ inventario = [
     }
 ]
 
-def getProducts(request):
+@csrf_exempt
+def Products(request):
     if request.method == "GET":
         return JsonResponse(inventario, safe=False)
+    if request.method == "POST":
+        global productID_
+        data = json.loads(request.body)
+        name = data.get("name")
+        minimunStock = data.get("minimunStock")
+        quantity = data.get("quantity") 
+        if (name is None or minimunStock is None or quantity is None) :
+            return JsonResponse({
+                "error": {
+                    "code": "ABC",
+                    "message": "Falta información",
+                    "details": "Falta información (name, minimunStock, quantity)"
+                }
+            }, status=400)
+        if (minimunStock < 0 or quantity < 0) :
+            return JsonResponse({
+                "error": {
+                    "code": "ABC",
+                    "message": "Cantidades deben ser mayores a cero",
+                    "details": "Cantidades: minimunStock, quantity deben ser mayores a cero"
+                }
+            }, status=400)
+        productID_ = productID_ + 1
+        inventario.append({
+            "productID": productID_,
+            "name": name,
+            "currentStock": quantity,
+            "minimunStock": minimunStock,
+            "lastUpdated": datetime.datetime.utcnow().isoformat()
+        })
+        return JsonResponse({
+            "success": {
+                "code": "ABC",
+                "message": "Se añadió correctamente",
+            }
+        }, status=201)
